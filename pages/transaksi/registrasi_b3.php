e0eb7172c8fd42c69c35e1e9ad20ff44f19c42f4
hak11 <abduljabar.hakim11@gmail.com>
2017-01-15 17:53:47 +0700

    update

--- pages/transaksi/registrasi_b3.php
+++ pages/transaksi/registrasi_b3.php
@@ -1,8 +1,15 @@
 <?php
 
 //START PREPARE DATA
+$query_report = "select
+                transaksi_registerb3.id_register as id_register,
+                master_perusahaan.nama_perusahaan as nama_perusahaan,
+                transaksi_registerb3.no_surat_regis as no_regis,
+                transaksi_registerb3.ltr_dtstart as ltr_dtstart from
+                transaksi_registerb3 left JOIN master_perusahaan
+                on transaksi_registerb3.id_perusahaan=master_perusahaan.id_perusahaan";
 
-$master_data_transaksi = mysql_query("SELECT * FROM `master_perusahaan`");
+$master_data_transaksi = mysql_query($query_report);
 $data_master_perusahaan = mysql_query("SELECT * FROM `master_perusahaan`");
 $data_master_pelabuhan = mysql_query("SELECT * FROM `master_pelabuhan` LIMIT 5");
 $data_master_negara = mysql_query("SELECT * FROM `master_negara`  LIMIT 5");
@@ -47,7 +54,8 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                                 <div class="col-sm-6">
                                     <div class="form-group form-float">
                                         <label class="form-label">Nama Perusahaan</label>
-                                        <select class="form-control show-tick" data-live-search="true">
+                                        <select class="form-control show-tick" data-live-search="true" name="kode_perusahaan" required>
+                                            <option value="">Pilih Perusahaan</option>
                                             <?php
                                             while ($data = mysql_fetch_array($data_master_perusahaan)) {
                                                 echo "
@@ -61,7 +69,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                                         <label class="form-label">Nomor Surat Registrasi</label>
 
                                         <div class="form-line">
-                                            <input type="text" class="form-control" name="nama_perusahaan">
+                                            <input type="text" class="form-control" name="no_surat_regis" required>
                                         </div>
                                     </div>
                                     <div class="input-group">
@@ -69,7 +77,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
 
                                         <div class="form-line">
                                             <input type="text" class="form-control" name="tanggal_berlaku"
-                                                   id="tanggal_berlaku" readonly="">
+                                                   id="tanggal_berlaku" readonly="" required>
                                         </div>
                                     </div>
                                 </div>
@@ -79,7 +87,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
 
                                         <div class="form-line">
                                             <input type="text" class="datepicker form-control" id="ltr_dtstart"
-                                                   name="ltr_dtstart">
+                                                   name="ltr_dtstart" required>
                                         </div>
                                     </div>
                                     <div class="input-group">
@@ -87,7 +95,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
 
                                         <div class="form-line">
                                             <input type="text" class="datepicker form-control" id="ltr_dtend"
-                                                   name="ltr_dtend" onChange="hitung_durasi()">
+                                                   name="ltr_dtend" onChange="hitung_durasi()" required>
                                         </div>
                                     </div>
                                     <div class="input-group">
@@ -95,7 +103,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
 
                                         <div class="form-line">
                                             <input type="text" class="form-control" name="ltr_duration"
-                                                   id="ltr_duration" readonly="">
+                                                   id="ltr_duration" readonly="" required>
                                         </div>
                                     </div>
                                 </div>
@@ -103,23 +111,79 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                             <div class="col-sm-12">
                                 <div class="table-responsive" style="overflow-x:auto;">
                                     <hr/>
-                                    <table class="table table-striped">
+                                    <table class="table table-striped table-bordered">
                                         <thead>
                                         <tr>
                                             <th>No</th>
                                             <th>Nama B3</th>
                                             <th>Nama Dagang</th>
-                                            <th>Jumlah Perimpor</th>
-                                            <th>Jumlah Impor Pertahun</th>
+                                            <th>Jml Perimpor</th>
+                                            <th>Jml Impor/Thn</th>
                                             <th>Negara Asal</th>
                                             <th>Pelabuhan Bongkar</th>
                                             <th>Nomor HS</th>
-                                            <th>Nomor Registrasi</th>
+<!--                                            <th>Nomor Registrasi</th>-->
                                         </tr>
                                         </thead>
+                                        <tfoot>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama B3</th>
+                                            <th>Nama Dagang</th>
+                                            <th>Jml Perimpor</th>
+                                            <th>Jml Impor/Thn</th>
+                                            <th>Negara Asal</th>
+                                            <th>Pelabuhan Bongkar</th>
+                                            <th>Nomor HS</th>
+                                            <!--                                            <th>Nomor Registrasi</th>-->
+                                        </tr>
+                                        </tfoot>
                                         <tbody id="table-dagang">
+                                        <tr>
+                                            <th scope="row">1</th>
+                                            <td>
+                                                <select name="namabe3[]" class="form-control" placeholder="Nama B3" required>
+                                                    <option value="">Pilih B3</option>
+                                                    <?php
+                                                    foreach($master_b3 as $data){
+                                                        ?>
+                                                        <option value="<?php echo $data['id'] ?>"><?php echo $data['name_b3'] ?></option>
+                                                        <?php
+                                                    }
+                                                    ?>
+                                                </select>
+                                            </td>
+                                            <td><input type="text" name="namaDagang[]"  class="form-control" placeholder="Nama Dagang" required></td>
+                                            <td><input type="text" name="jmlImport[]"  class="form-control" placeholder="Jml Import" required style="max-width: 100px;"></td>
+                                            <td><input type="text" name="jmlImportTahunan[]"  class="form-control" placeholder="Jml/Thn" required style="max-width: 100px;"></td>
+                                            <td>
+                                                <select name="negara[]"  class="form-control" placeholder="Negara Asal" required>
+                                                    <option value="">Negara</option>
+                                                    <?php
+                                                    foreach($master_negara as $data){
+                                                        ?>
+                                                        <option value="<?php echo $data['kode_negara'] ?>"><?php echo $data['nama_negara'] ?></option>
+                                                        <?php
+                                                    }
+                                                    ?>
+                                                </select>
+                                            <td>
+
+                                                <select name="pelabuhan[]"  class="form-control" placeholder="Pelabuhan Bongkar" required>
+                                                    <option value="">Pelabuhan</option>
+                                                    <?php
+                                                    foreach($master_pelabuhan as $data){
+                                                        ?>
+                                                        <option value="<?php echo $data['id_pelabuhan'] ?>"><?php echo $data['kode_pelabuhan'] ?></option>
+                                                        <?php
+                                                    }
+                                                    ?>
+                                                </select>
+                                            <td><input type="text" name="nomorHS[]"  class="form-control" placeholder="Nomor HS" required></td>
+<!--                                            <td><input type="text" name="nomorReg[]"  class="form-control" placeholder="Nomor Register" required></td>-->
+                                        </tr>
                                         <?php
-                                        for($i=1; $i<=10; $i++ ){
+                                        for($i=2; $i<=5; $i++ ){
 
                                             ?>
                                         <tr>
@@ -137,8 +201,8 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                                                 </select>
                                             </td>
                                             <td><input type="text" name="namaDagang[]"  class="form-control" placeholder="Nama Dagang"></td>
-                                            <td><input type="text" name="jmlImport[]"  class="form-control" placeholder="Jml Import"></td>
-                                            <td><input type="text" name="jmlImportTahunan[]"  class="form-control" placeholder="Jml/Thn"</td>
+                                            <td><input type="text" name="jmlImport[]"  class="form-control" placeholder="Jml Import"  style="max-width: 100px;"></td>
+                                            <td><input type="text" name="jmlImportTahunan[]"  class="form-control" placeholder="Jml/Thn"  style="max-width: 100px;"></td>
                                             <td>
                                                 <select name="negara[]"  class="form-control" placeholder="Negara Asal">
                                                     <option value="">Negara</option>
@@ -163,7 +227,7 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                                                     ?>
                                                 </select>
                                             <td><input type="text" name="nomorHS[]"  class="form-control" placeholder="Nomor HS"></td>
-                                            <td><input type="text" name="nomorReg[]"  class="form-control" placeholder="Nomor Register"></td>
+<!--                                            <td><input type="text" name="nomorReg[]"  class="form-control" placeholder="Nomor Register"></td>-->
                                         </tr>
                                         <?php
                                         }
@@ -184,41 +248,40 @@ while($row=mysql_fetch_array($data_master_pelabuhan)){
                         <table class="table table-bordered table-striped table-hover js-basic-example dataTable">
                             <thead>
                             <tr>
+                                <th>No</th>
+                                <th>No Reg</th>
                                 <th>Nama Perusahaan</th>
-                                <th>Alamat</th>
-                                <th>Bidang Usaha</th>
-                                <th>Jenis Importir</th>
-                                <th>Nama PIC</th>
-                                <th>Telepon PIC</th>
+                                <th>Tangggal Surat</th>
                                 <th>Aksi</th>
                             </tr>
                             </thead>
                             <tfoot>
                             <tr>
+                                <th>No</th>
+                                <th>No Reg</th>
                                 <th>Nama Perusahaan</th>
-                                <th>Alamat</th>
-                                <th>Bidang Usaha</th>
-                                <th>Jenis Importir</th>
-                                <th>Nama PIC</th>
-                                <th>Telepon PIC</th>
+                                <th>Tangggal Surat</th>
                                 <th>Aksi</th>
                             </tr>
                             </tfoot>
                             <tbody>
                             <?php
+                            $nomer = 1;
                             while ($list = mysql_fetch_array($master_data_transaksi)) {
                                 echo "
 									<tr>
+										<td>" . $nomer . "</td>
+										<td>" . $list['no_regis'] . "</td>
 										<td>" . $list['nama_perusahaan'] . "</td>
-										<td>" . $list['alamat_perusahaan'] . "</td>
-										<td>" . $list['bidang_usaha'] . "</td>
-										<td>" . $list['jenis_importir'] . "</td>
-										<td>" . $list['nama_pic'] . "</td>
-										<td>" . $list['tlp_pic'] . "</td>
-										<td><a class='btn btn-default' href='index.php?halaman=edit_perusahaan&id=" . $list['id_perusahaan'] . "'>Ubah Data</a> <a class='btn btn-danger' href='action/master_perusahaan/delete.php?id_perusahaan=" . $list['id_perusahaan'] . "' onClick='return warning()'>Hapus</a></td>
+										<td>" . $list['ltr_dtstart'] . "</td>
+										<td>
+										    <a class='btn btn-default' href='#'>Lihat</a>
+										    <a class='btn btn-danger' href='#' onClick='return warning()'>Hapus</a>
+										</td>
 									</tr>
 									";
                                 # code...
+                                $nomer++;
                             }
 
                             ?>
--- pages/transaksi/tambahRegis.php
+++ pages/transaksi/tambahRegis.php
@@ -1,6 +1,43 @@
 <?php
-echo "<pre>";
-print_r($_POST);
-echo "</pre>";
+require "../../database/koneksi.php";
+$id_perusahaan = $_POST['kode_perusahaan'];
+$no_surat_regis = $_POST['no_surat_regis'];
+$tanggal_berlaku = date("Y-m-d", strtotime($_POST['tanggal_berlaku']));
+$ltr_dtstart = date("Y-m-d", strtotime($_POST['ltr_dtstart']));
+$ltr_dtend = date("Y-m-d", strtotime($_POST['ltr_dtend']));
+$ltr_duration = $_POST['ltr_duration'];
+$created_at = date('Y-m-d');
+$queryMain = "INSERT INTO transaksi_registerb3 (`id_perusahaan`,`no_surat_regis`,`tanggal_berlaku`,`ltr_dtstart`,`ltr_dtend`,`ltr_duration`,`created_at`)
+              VALUES ('$id_perusahaan','$no_surat_regis','$tanggal_berlaku','$ltr_dtstart','$ltr_dtend','$ltr_duration','$created_at')";
+if(mysql_query($queryMain)){
+    $id_register=mysql_insert_id();
+    if($id_register){
+        $forParam = $_POST['namabe3'];
+        $key = 0;
+        foreach($forParam as $data ){
+            if($data){
+            $id_b3 = $_POST['namabe3'][$key];
+            $namaDagang = $_POST['namaDagang'][$key];
+            $jmlImport = $_POST['jmlImport'][$key];
+            $jmlImportTahunan = $_POST['jmlImportTahunan'][$key];
+            $id_negara = $_POST['negara'][$key];
+            $id_pelabuhan = $_POST['pelabuhan'][$key];
+            $nomorHS = $_POST['nomorHS'][$key];
+            $nomorReg = $_POST['nomorReg'][$key];
+
+            $queryDetails = "INSERT INTO register_details (`id_register`,`id_b3`,`namaDagang`,`jmlImport`,`jmlImportTahunan`,`id_negara`,
+                              `id_pelabuhan`,`nomerHS`,`nomerReg`) VALUES ('$id_register','$id_b3','$namaDagang','$jmlImport','$jmlImportTahunan',
+                              '$id_negara','$id_pelabuhan','$nomorHS','$nomorReg')";
+
+            mysql_query($queryDetails);
+            }
+            $key++;
+        }
+        header('Location: ../../index.php?halaman=registrasi_b3');
+    }
+} else {
+    echo "Terdapat Kesalahan";
+    exit;
+}
 
 ?>
